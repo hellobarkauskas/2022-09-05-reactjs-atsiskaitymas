3d9b49702afd5d8777735fb46d03d02241ff44a7
Martynas <martynas@Martynass-MBP.lan>
2022-09-05 21:09:19 +0300

    add Register component to test routes

---
+++ src/components/RegisterComponent.jsx
@@ -0,0 +1,12 @@
+
+function RegisterForm() {
+    return (
+        <div>
+            <label for='register'>Register</label>
+            <input type="text" name='register' />
+
+        </div>
+    )
+}
+
+export default RegisterForm;

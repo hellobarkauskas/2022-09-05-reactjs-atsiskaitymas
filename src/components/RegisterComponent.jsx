4e16f6878f3da9af01cfc80ff3ab8e48867e30fe
Martynas <martynas@Martynass-MBP.lan>
2022-09-06 22:24:29 +0300

    add minor register component style update

--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -32,12 +32,14 @@ function RegisterForm() {
             <MainLayout />
 
             <form className="register-form">
-                <h2>Register page</h2>
-                <label for='email'>Email</label>
-                <input type="text" name='email' id="email" value={email} onChange={event => setEmail(event.target.value)} />
-                <label for='password'>Password</label>
-                <input type="text" name='password' id="password" value={password} onChange={event => setPassword(event.target.value)} />
-                <button type="submit" onClick={event => RegisterUser(event)}>Register</button>
+                <div className="register-form-style">
+                    <h2>Register page</h2>
+                    <label for='email'>Email</label>
+                    <input type="text" name='email' id="email" value={email} onChange={event => setEmail(event.target.value)} />
+                    <label for='password'>Password</label>
+                    <input type="text" name='password' id="password" value={password} onChange={event => setPassword(event.target.value)} />
+                    <button type="submit" onClick={event => RegisterUser(event)}>Register</button>
+                </div>
             </form>
         </div>
     )
